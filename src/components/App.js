b01387e37f3c48da7423e333e58a9c1e29a4261f
SARUN <>
2019-09-16 16:52:27 +0700

    no message

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,38 @@
 import React from 'react';
 import styled from 'styled-components'
 
-const Button = styled.button`background: transparent;
+const Button = styled.button`
+background: transparent;
 border-radius: 3px;
 border: 2px solid palevioletred;
 color: palevioletred;
 margin: 0 1em;
 padding: 0.25em 1em;`
 
+const Wrap = styled.div`
+position: relative;
+display: flex;
+flex-direction: column;
+justify-content: center;
+padding: 2em 20em;
+`
+
 function App() {
   return (
-    <Button>
-      ddddd
-    </Button>
+    <div>
+      <Wrap>
+        <Button>
+          Button
+        </Button>
+      </Wrap>
+      <Wrap>
+        <Button>
+          Button
+        </Button>
+      </Wrap>
+      
+    </div>
+    
   );
 }
 
